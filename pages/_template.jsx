e8e47e0b61154a6656afe725e84b0cd29ff4c56d
cerico <garethrobertlee@gmail.com>
2017-10-02 15:16:18 -0400

    byline

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -16,6 +16,12 @@ class Template extends React.Component {
  
   
   render () {
+
+    const style = {
+      byline: {
+        color: '#eadfc1'
+      }
+    }
     const { children } = this.props
     return (
       <div>
@@ -23,6 +29,7 @@ class Template extends React.Component {
         <Headroom >
         <div role="hero" className="hero">
         <Link  to="/" className="inverse-topper">Cerico</Link>
+        <h4 style={style.byline}>Javascript, Docker, Elixir, Nginx, and other fun and games</h4>
         </div>
         <svg width="100%" height="47px" viewBox="0 0 1440 47" version="1.1" xmlns="http://www.w3.org/2000/svg" className="hero__div" preserveAspectRatio="none"><defs data-reactid="15"></defs><g id="Page-1" stroke="none" strokeWidth="1" fill="#1e1e1e" fillRule="evenodd" data-reactid="16"><path d="M0,0.0577405639 C117.504588,18.44359 269.602477,22.1720696 456.293666,11.2431795 C562.076057,5.05068514 730.784198,0.911127653 885.297232,3.27366179 C1157.17617,7.43074321 1386.98062,21.3276838 1440,38.3891927 L1440,46.9388979 L0,46.9388979 L0,0.0577405639 Z" id="Path-9" fill="#1e1e1e" data-reactid="17"></path></g>
         </svg>
